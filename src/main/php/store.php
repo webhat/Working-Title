d2970e9c9e4a6c9963ec1cd6d113596f1c922289
webhat <daniel.crompton@gmail.com>
2012-11-08 13:20:07 +0000

    Added mongo backup script Added store

---
+++ src/main/php/store.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'loggedinas.php';
+
+foreach( $_POST as $prop => $val) {
+	$storedprop = (string) $prop;
+	$storedval = (string) $val;
+	$doc = new DOMDocument();
+
+	// load the HTML string we want to strip
+	$doc->loadHTML($storedval);
+
+	// get all the script tags
+	$script_tags = $doc->getElementsByTagName('script');
+
+	$length = $script_tags->length;
+
+	// for each tag, remove it from the DOM
+	for ($i = 0; $i < $length; $i++) {
+		  $script_tags->item(0)->parentNode->removeChild($script_tags->item(0));
+	}
+
+	// get the HTML string back
+	$no_script_html_string = $doc->saveHTML();
+	$ul->setProperty($storedprop, $no_script_html_string);
+}
+$ul->store();
+
+?>
