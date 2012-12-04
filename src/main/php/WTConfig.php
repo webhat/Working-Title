179a099854ddff111733f1eda13d38e242ed232e
Daniël W. Crompton <daniel.crompton@gmail.com>
2012-12-04 12:14:39 +0100

    Corrected stupid commit of username/apikey/password... Argh! #gklst

--- src/main/php/WTConfig.php
+++ src/main/php/WTConfig.php
@@ -9,14 +9,14 @@ class WTConfig {
 
 	function __construct() {
 		$this->mailchimp = array(
-				"listid"	=> "c5774f1303",
-				"apikey"	=> "df529fb3ed1cf3d90ff9dbba86924add-us5"
+				"listid"	=> "",
+				"apikey"	=> ""
 		);
 		$this->mandrill = array(
 				"host"			=> "smtp.mandrillapp.com",
 				"port"			=> "587",
-				"username"	=> "oid::b00f079660447361efceeba41ad3904b606c518a",
-				"password"	=> "8fe0fbac-5b5c-49bf-92be-7deee013c8fc"
+				"username"	=> "",
+				"password"	=> ""
 		);
 		$this->service = array(
 				"name"	=> "WorkingTitle365",
