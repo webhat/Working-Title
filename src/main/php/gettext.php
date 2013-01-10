0ab853461418f21db1fe2c8a49dbb2b1dc2113e6
webhat <daniel.crompton@gmail.com>
2013-01-10 16:59:19 +0000

    Added locales: broken for en_GB

--- src/main/php/gettext.php
+++ src/main/php/gettext.php
@@ -1,15 +1,20 @@
 <?php
 
-define("LOCALE_DIR", "/home/ec2-user/beta/Working-Title/src/main/locale");
-define("LOCALE", "nl_NL.utf8");
+require_once('bootstrap.php');
 
-putenv('LC_ALL='. LOCALE);
-setlocale(LC_ALL, 0);
-setlocale(LC_CTYPE, 0);
+define("LOCALE_DIR", "/home/ec2-user/beta/Working-Title/src/main/locale");
+$locale = Locale::detectLanguage("0.0.0.0", $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+define("LOCALE", $locale['locale'] .".utf8");
+?><!-- <?= LOCALE ?> --><?php
+?><!-- <?= $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?> --><?php
+
+define("LANG",preg_replace("/_.*/","",LOCALE));
+
+//putenv('LC_ALL='. LOCALE);
 setlocale(LC_ALL, LOCALE);
 
 bindtextdomain("messages", LOCALE_DIR);
-bind_textdomain_codeset("messages", 'UTF-8');
+bind_textdomain_codeset("messages", 'UTF8');
 
 textdomain("messages");
 
