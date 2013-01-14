be4ad7bb0f51196e641264e195f61cb3b4d6105e
webhat <daniel.crompton@gmail.com>
2013-01-14 22:13:12 +0000

    AB Testing FB

--- src/main/js/abtests.js
+++ src/main/js/abtests.js
@@ -1,3 +1,12 @@
 console.log("Tests Loading");
-//$(".fb-login-button").show();
+$(document).ready(function() {
+		setTimeout(
+			function() {
+				$("#fblogin").css("display","inline-block");
+				$("#fblogin").click(function() {
+						_gaq.push(['_trackEvent', 'SocialLogin', 'FaceBook']);
+					});
+				console.log("Show fblogin");
+			}, 2000);
+});
 console.log("Tests Loaded");
