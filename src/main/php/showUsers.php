fd01d2ba3bd4b237b93c46ea020286c7d9769e61
webhat <daniel.crompton@gmail.com>
2013-02-05 15:43:36 +0000

    Changed showUsers to include gravatar [ci skip]

--- src/main/php/showUsers.php
+++ src/main/php/showUsers.php
@@ -8,10 +8,13 @@ $users = new Users();
 $userlist = $users->getUsers();
 
 foreach($userlist as $user) {
+		$grav = md5(strtolower(trim($user['mail'])));
 	?>
 		<a href="http://workingtitle365.com/maker/<?php
 		echo $user['username'];
-	?>"><?php
+	?>">
+		<img width="40" height="40" src="http://www.gravatar.com/avatar/<?= $grav ?>" />
+		<?php
 		echo $user['username'];
 	?></a>&nbsp;&nbsp;&nbsp;<em><?php echo $user['mail'] ?></em><br /><?php
 }
