d5f2fd4bbd4deb2627e6c7bbd0fd434e60493a97
webhat <daniel.crompton@gmail.com>
2013-03-06 14:47:22 +0000

    ga for mandrill

--- src/main/php/mailmyfans.php
+++ src/main/php/mailmyfans.php
@@ -45,8 +45,14 @@ $to = array( "name" => $maker, "mail" => $mailaddr);
 
 $mail = new Mail();
 
-$mail->send($to, $mailmessage);
+//$mail->send($to, $mailmessage);
 
 echo $mailmessage['body'];
 
 ?>
+        <script>
+            var _gaq=[['_setAccount','UA-34506988-1'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
